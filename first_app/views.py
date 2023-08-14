662d3a4dc1912bb194dfc9734db895d4c159f8a0
Nahidul Islam Shanto <nahidulislamshantogo@gmail.com>
2023-08-14 21:01:09 +0600

    update

--- first_app/views.py
+++ first_app/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, './first_app/index.html')
+    return render(request, '/index.html')
    
